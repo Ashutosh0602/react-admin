e886d1f9961646520a4a14e4244644fc27e07395
Ashutosh0602 <ashu00jnp@gmail.com>
2022-12-25 20:35:09 +0530

    commit 2

--- src/scenes/global/Sidebar.js
+++ src/scenes/global/Sidebar.js
@@ -1,5 +1,6 @@
-import React from "react";
-
-export const Sidebar = () => {
+import { React, useState } from "react";
+import { Menu, MenuItem, ProSidebarProvider } from "react-pro-sidebar";
+import "react-pro-sidebar/dist/css/styles.css";
+export default Sidebar = () => {
   return <div>Sidebar</div>;
 };
